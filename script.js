323479167a92b12d7a7a051074e8197de80007a0
Osbourne34 <evolkov1804@gmail.com>
2022-01-16 23:56:19 +0500

    home work lesson02

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const title;
-const screens;
-const screenPrice;
-const rollback;
-const fullPrice;
-const adaptive;
+const title = 'Name project';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 50;
+const rollback = 10;
+const fullPrice = 500;
+const adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
+console.log(screens.toLowerCase().split(', '));
+console.log(fullPrice * (rollback / 100));
