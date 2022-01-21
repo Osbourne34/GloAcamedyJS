2c48769e0c467254ae7ec477029b62ca96dafba9
Osbourne34 <evolkov1804@gmail.com>
2022-01-22 00:53:18 +0500

    lesson03 home work

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-const title = 'Name project';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 50;
-const rollback = 10;
-const fullPrice = 500;
-const adaptive = true;
+const title = prompt('Как называется ваш проект?'); // +
+const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные'); // +
+const adaptive = confirm('Нужен ли адаптив на сайте?'); // +
+const screenPrice = +prompt('Сколько будет стоить данная работа?'); // +
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
-console.log(screens.toLowerCase().split(', '));
-console.log(fullPrice * (rollback / 100));
+const service1 = prompt('Какой дополнительный тип услуги нужен?'); // +
+const servicePrice1 = +prompt('Сколько это будет стоить?'); // +
+const service2 = prompt('Какой дополнительный тип услуги нужен?'); // +
+const servicePrice2 = +prompt('Сколько это будет стоить?'); // +
+
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const rollback = 10;
+
+const servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
+
+if (fullPrice >= 30000) {
+    alert(`${fullPrice}p. Даем скидку 10%`);
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+    alert(`${fullPrice}p. Даем скидку 5%`);
+} else if (fullPrice < 15000 && fullPrice > 0) {
+    alert(`${fullPrice}p. Скидка не предусмотрена`);
+} else if (fullPrice <= 0) {
+    alert('Что то пошло не так');
+}
+
+console.log(title);
+console.log(screens);
+console.log(screenPrice);
+console.log(adaptive);
+console.log(fullPrice);
+console.log(rollback);
+console.log(servicePercentPrice);
